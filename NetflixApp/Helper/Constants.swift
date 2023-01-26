dbee0745b4aff92ece7872e51736cbffb98cbfbf
bhanu734 <bhanu734@gmail.com>
2023-01-25 23:40:17 -0800

    carousal collectionview sizeupdated

--- NetflixApp/Helper/Constants.swift
+++ NetflixApp/Helper/Constants.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Mac on 24/12/22.
 //
-import Foundation
+
 import UIKit
 
 let email_UD = "email_UD"
@@ -13,7 +13,7 @@ let email_UD = "email_UD"
 let Home_tile_Height: CGFloat = UIScreen.main.bounds.width*0.4
 let Home_Tile_Width: CGFloat = UIScreen.main.bounds.width*0.4
 
-let Home_carousal_Height: CGFloat = (UIScreen.main.bounds.width*0.4) + 40
+let Home_carousal_Height: CGFloat = (UIScreen.main.bounds.width*0.4) + 50
 //let Home_carousal_Width: CGFloat = (UIScreen.main.bounds.width*0.4) + 45
 
-let Home_banner_Height: CGFloat = UIScreen.main.bounds.height*0.75
+let Home_banner_Height: CGFloat = UIScreen.main.bounds.height*0.70
