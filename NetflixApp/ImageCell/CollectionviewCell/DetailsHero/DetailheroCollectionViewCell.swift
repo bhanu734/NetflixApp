9f5740849eb6b16d1f677ddf74dbcbb905a555e8
bhanu734 <bhanu734@gmail.com>
2023-02-06 21:06:35 -0800

    details hero configuration partially done

---
+++ NetflixApp/ImageCell/CollectionviewCell/DetailsHero/DetailheroCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailheroCollectionViewCell.swift
+//  NetflixApp
+//
+//  Created by Mac on 06/02/23.
+//
+
+import UIKit
+
+class DetailheroCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
